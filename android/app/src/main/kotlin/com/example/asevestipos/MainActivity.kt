7737d9515a769343c9cc162a851d8bc1eebf973a
TeaWrexxx <jumkloe@gmail.com>
2023-12-02 08:43:41 +0800

    Initial Commit

---
+++ android/app/src/main/kotlin/com/example/asevestipos/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.asevestipos
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/components/dimensions.dart
@@ -0,0 +1,2 @@
+// set device dimensions here for mobile / tablet / desktop
+const mobileWidth = 550;
